a7d5d227c693c344d4de0095f3e1d691749865b6
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2022-06-10 13:39:23 +0900

    Fsm: 仕様変更
    * get_all_eqpairs(), get_all_eqgroups() 追加

--- lctools/eqtable.py
+++ lctools/eqtable.py
@@ -48,7 +48,7 @@ class EqTable:
         else:
             return None
 
-    def get_all(self):
+    def get_all_pairs(self):
         """等価状態対を全て取り出す．
 
         :return: 等価状態対のリストを返す．
@@ -58,6 +58,35 @@ class EqTable:
         for s, t, step in self.__marks.keys():
             if step == self.__max_step:
                 ans_list.append((s, t))
+        return ans_list
+
+    def get_all_groups(self):
+        """等価状態グループをすべて取り出す．
+        """
+        rep_dict = {}
+        state_set = set()
+        for s, t, step in self.__marks.keys():
+            if step == self.__max_step:
+                assert s < t
+                state_set.add(s)
+                state_set.add(t)
+                if s not in rep_dict:
+                    rep_dict[s] = s
+                elif rep_dict[s] > s:
+                    rep_dict[s] = s
+                if t not in rep_dict:
+                    rep_dict[t] = s
+                elif rep_dict[t] > s:
+                    rep_dict[t] = s
+        group_dict = {}
+        for s in state_set:
+            r = rep_dict[s]
+            if r not in group_dict:
+                group_dict[r] = list()
+            group_dict[r].append(s)
+        ans_list = list()
+        for s_list in group_dict.values():
+            ans_list.append(s_list)
         return ans_list
 
     def put(self, s, t, step, cond_set):
